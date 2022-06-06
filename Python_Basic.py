25bfe37405f5ba0e3499d64683b613956ff2f26b
gowtham659 <gowthamthota121@gmail.com>
2022-06-06 19:58:51 +0530

    final changes

--- Python_Basic.py
+++ Python_Basic.py
@@ -1,9 +1,14 @@
 class student:
-    def __init__(self,name,age) -> None:
+    def __init__(self,name,age,dept) -> None:
         self.name = name
         self.age = age
+        self.dept = dept
     def details(self):
         print("My name is %s and age is %d"%(self.name,self.age))
+    
+    def depart(self):
+        print("Belongs to the Department %s"%(self.dept))
 
-std = student("gowtham",89)
+std = student("gowtham",89,"IT")
 std.details()
+std.depart()
